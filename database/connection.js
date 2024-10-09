e7a6eeff52454cea2fcaf5ae54c6733e394241ee
DR Dev <dromeroldev@gmail.com>
2024-10-08 20:08:52 -0500

    Creation of index.js, configure connection to the database

---
+++ database/connection.js
@@ -0,0 +1,17 @@
+import { connect } from "mongoose";
+import dotenv from "dotenv";
+
+// Configurar dotenv para usar las variables de entorno
+dotenv.config();
+
+const connection = async() => {
+    try {
+        await connect(process.env.MONGODB_URI);
+        console.log("Conectado corrrectamente a DB_Social_Network");
+    } catch (error) {
+        console.log("Error al conectar la BD", error);
+        throw new Error("No fue posible conectarse a la base de datos :(")
+    }
+}
+
+export default connection;
---
+++ index.js
@@ -0,0 +1,41 @@
+// Importar dependencias (configurar en package.json)
+import express from "express";
+import connection from "./database/connection.js"
+import cors from "cors";
+import bodyParser from "body-parser";
+import UserRoutes from "./routes/users.js"
+import PublicationRoutes from "./routes/publications.js"
+import FollowRoutes from "./routes/follows.js"
+
+// Mensaje de bienvenida de confirmación de conexión
+connection();
+
+
+// Crear el servidor Node
+const app = express();
+const puerto = process.env.PORT || 3900;
+
+// Configurar cors para que acepte peticiones del frontend
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionSuccessStatus: 204
+}));
+
+// Decodificar los datos desde lso formularios para convertirlos en objetos de Javascripts
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Configurar rutas del aplicativo (modulos)
+app.use('/api/user', UserRoutes);
+app.use('/api/publication', PublicationRoutes);
+app.use('/api/follow', FollowRoutes);
+
+
+// Configurar el servidor de Node
+app.listen(puerto, () => {
+    console.log("Servidor de Node ejecutando! Puerto: ", puerto);
+});
+
+export default app;
